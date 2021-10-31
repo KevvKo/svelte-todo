524b7c5b9198e690912e8d877b79318b8e665f33
KevvKo <kklein915@gmail.com>
2021-10-31 16:37:22 +0100

    add tailwind and header for the todo app

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header class='bg-purple-500 text-white p-2 flex'>
+    <span class='font-semibold'>
+        Svelte-ToDo        
+    </span>
+    <a class='ml-auto' href='https://github.com/KevvKo/svelte-todo'>
+        <img src='/GitHub-Mark-Light-32px.png' alt='Github'>
+    </a>
+</header>
+
+<div class='w-11/12 m-auto border-solid border-2 border-purple-500 rounded mt-10'>
+    <table>
+        <thead>
+            <tr>
+                <td>
+                    To Do´s
+                </td>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+</div>
+
+<style global>
+    @import '../css/styles.css';
+</style>
