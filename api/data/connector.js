59eb8ceb2ca8030ed2e0e543079d726c08e34039
KevvKo <kklein915@gmail.com>
2021-11-05 18:28:42 +0100

    changes for db connection

--- api/data/connector.js
+++ api/data/connector.js
@@ -1,13 +1,25 @@
 const { MongoClient } = require('mongodb');
-const { API_URI } = require('dotenv').config();
+require('dotenv').config()
 
-const client = new MongoClient( API_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const client = new MongoClient( process.env.API_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+async function run(){
+  client.connect(async err => {
+
+    const collection = client.db("ToDoApp").collection("ToDos");
+    // perform actions on the collection object
+    const cursor = await collection.find({text:'shopping'})
+    if ((await cursor.count()) === 0) {
+
+      console.log("No documents found!");
+    }
+    await cursor.forEach(console.dir);
+    client.close();
+  });
+
+}
+
+run()
 module.export = {
-    client
+    run
 }
--- api/server.js
+++ api/server.js
@@ -2,10 +2,13 @@
 
 const { ApolloServer } = require('apollo-server');
 const { schema } = require('./grapql/schema');
+const { MongoClient } = require("mongodb");
+
 
 const server = new ApolloServer({ 
     schema: schema
 });
+
 
 server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
